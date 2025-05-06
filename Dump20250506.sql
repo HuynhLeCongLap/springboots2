839e1a49e9e9f68ab9988955935b5a18bfbcb23c
HuynhLeCongLap <164972544+HuynhLeCongLap@users.noreply.github.com>
2025-05-06 22:33:34 +0700

    Update Dump20250506.sql

--- Dump20250506.sql
+++ Dump20250506.sql
@@ -114,3 +114,4 @@ CREATE TABLE `users_roles` (
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2025-05-06 22:29:44
+ INSERT INTO users (username, password, role) VALUES ('admin', '$2a$10$ChE3oJpHvdHmZWJ.vU4o3e4irXnM.GPb1SQIFd9ONqGizLqBlo3bK', 'ADMIN');
