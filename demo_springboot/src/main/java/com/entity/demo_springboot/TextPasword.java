04da8c7c1cfa907100f1c2d52de1e74de01cd03f
HuynhLeCongLap <164972544+HuynhLeCongLap@users.noreply.github.com>
2025-05-06 22:22:50 +0700

    Delete demo_springboot directory

--- demo_springboot/src/main/java/com/entity/demo_springboot/TextPasword.java
+++
@@ -1,9 +0,0 @@
-package com.entity.demo_springboot;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class TextPasword {
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("123456"));
-    }
-}
